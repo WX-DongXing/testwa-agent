fc14d6f815b5deb073211fbdcd5cee96103c1913
xd <xingdong.com@live.com>
2018-08-18 15:58:08 +0800

    Test data control with redux

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,7 +1,4 @@
 import React, {Component} from 'react';
-import {createSelector} from 'reselect';
-import {updateUser} from './actions/userAction';
-import { connect } from 'react-redux';
 import {Switch, Route, withRouter} from 'react-router-dom';
 import '././style.scss';
 import Config from './component/config';
@@ -20,20 +17,4 @@ class App extends Component {
   }
 }
 
-const userSelector = createSelector(
-  state => state.user,
-  user => user
-)
-
-const mapStateToProps = createSelector(
-  userSelector,
-  (user) => ({
-    user
-  })
-)
-
-const mapActionsToProps = {
-  onUpdateUser: updateUser
-}
-
-export default withRouter(connect(mapStateToProps, mapActionsToProps)(App));
+export default withRouter(App)
--- src/renderer/component/login.js
+++ src/renderer/component/login.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {createSelector} from 'reselect';
+import {updateUser} from '../actions/userAction';
+import connect from 'react-redux/es/connect/connect';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.updateUser = this.updateUser.bind(this);
+  }
+  updateUser(event) {
+    this.props.onUpdateUser(event.target.value);
+  }
   render() {
     return (
       <div>
         <p>login</p>
-        <Link to='/config'>config</Link>
+        <input type="text" onChange={this.updateUser}/>
+        {this.props.user}
+        <Link to='/config'>go to config</Link>
       </div>
     )
   }
 }
 
-export default Login;
+const userSelector = createSelector(
+  state => state.user,
+  user => user
+)
+
+const mapStateToProps = createSelector(
+  userSelector,
+  (user) => ({
+    user
+  })
+)
+
+const mapActionsToProps = {
+  onUpdateUser: updateUser
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(Login);
