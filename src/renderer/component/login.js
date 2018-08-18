5b0c9032a8742f07321b4a75651fea9e1a7dbc50
xd <xingdong.com@live.com>
2018-08-18 18:30:13 +0800

    Add cookie control

--- src/renderer/component/login.js
+++ src/renderer/component/login.js
@@ -3,15 +3,23 @@ import {Link} from 'react-router-dom';
 import {createSelector} from 'reselect';
 import {updateUser} from '../actions/userAction';
 import connect from 'react-redux/es/connect/connect';
+import {Cookie} from '../cookie';
+const cookie = new Cookie();
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.updateUser = this.updateUser.bind(this);
+    cookie.set('name1', '1', 10);
+    cookie.set('name2', 2, 10);
+    cookie.set('name3', 'a', 10);
+    setTimeout(() => {console.log(cookie.getAll())}, 4)
   }
+
   updateUser(event) {
     this.props.onUpdateUser(event.target.value);
   }
+
   render() {
     return (
       <div>
---
+++ src/renderer/cookie.js
@@ -0,0 +1,49 @@
+export class Cookie {
+  set(name, value, exDays) {
+    const now = new Date();
+    now.setTime(now.getTime() + (exDays * 1000));
+    let expires = `expires=${now.toGMTString()}`;
+    document.cookie = `${name}=${value}; ${expires}`;
+  }
+
+  setAll(cookies) {
+
+  }
+
+  get(name) {
+    let target = name + '=';
+    const cookies = document.cookie.split(';');
+    for(let cookie of cookies) {
+      if (cookie.trim().indexOf(target) === 0) return cookie.trim().substring(target.length, cookie.trim().length)
+    }
+    return '';
+  }
+
+  getAll() {
+    let target = name + '=';
+    const cookies = {};
+      document.cookie.split(';').map(value => {
+      let item = value.trim().split('=');
+      let str1 = `{"${item[0]}":`;
+      let str2 = isNaN(Number(item[1])) ? `"${item[1]}"` : item[1]
+      let str3 = '}';
+      Object.assign(cookies, JSON.parse(str1 + str2 + str3))
+    })
+    return cookies;
+  }
+
+  remove(name) {
+    let target = name + '=';
+    const cookies = document.cookie.split(';');
+    for(let cookie of cookies) {
+      if (cookie.trim().indexOf(target) === 0) {
+        document.cookie = `${target}; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+        break;
+      }
+    }
+  }
+
+  removeAll() {
+
+  }
+}
