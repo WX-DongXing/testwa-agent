16bd97a22f39ac5dd663eb9510dc6e87997650f3
xd <xingdong.com@live.com>
2018-08-29 16:12:42 +0800

    Modify theme style

--- src/renderer/component/ternimal/terminal.js
+++ src/renderer/component/ternimal/terminal.js
@@ -1,26 +1,41 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
-import './terminal.scss'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import FiberManualRecordRoundedIcon from '@material-ui/icons/FiberManualRecordRounded';
 import PlayCircleOutlineRoundedIcon from '@material-ui/icons/PlayCircleOutlineRounded';
 import PauseCircleOutlineRoundedIcon from '@material-ui/icons/PauseCircleOutlineRounded';
 import ClearAllRoundedIcon from '@material-ui/icons/ClearAllRounded';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import PaletteRoundedIcon from '@material-ui/icons/PaletteRounded';
+import './terminal.scss'
 
 class Terminal extends Component {
   constructor(props) {
     super(props)
-    this.state = { run: false, color: 'terminal-wrap primary' }
+    this.state = { run: false, color: 'terminal-wrap primary', anchorEl: null }
     this.run = this.run.bind(this)
+    this.openTheme = this.openTheme.bind(this)
+    this.closeTheme = this.closeTheme.bind(this)
   }
 
   run() {
     this.setState({ run: !this.state.run })
   }
 
+  openTheme(event) {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  closeTheme() {
+    this.setState({ anchorEl: null })
+  }
+
   setColor(color) {
     this.setState({ color: color })
+    this.closeTheme()
   }
 
   render() {
@@ -32,15 +47,52 @@ class Terminal extends Component {
               <HomeRoundedIcon />
             </IconButton>
           </Link>
-          <IconButton aria-label="white" className="icon-button white" onClick={this.setColor.bind(this, 'terminal-wrap light')}>
-            <FiberManualRecordRoundedIcon />
+          <IconButton className="icon-button theme"
+                      aria-label="theme"
+                      aria-owns={Boolean(this.state.anchorEl) ? 'long-menu' : null}
+                      aria-haspopup="true"
+                      onClick={this.openTheme}
+          >
+            <PaletteRoundedIcon />
           </IconButton>
-          <IconButton aria-label="primary" className="icon-button primary" onClick={this.setColor.bind(this, 'terminal-wrap primary')}>
-            <FiberManualRecordRoundedIcon />
-          </IconButton>
-          <IconButton aria-label="dark" className="icon-button" onClick={this.setColor.bind(this, 'terminal-wrap dark')}>
-            <FiberManualRecordRoundedIcon />
-          </IconButton>
+          <Menu className="icon-menu"
+                id="long-menu"
+                anchorEl={this.state.anchorEl}
+                open={Boolean(this.state.anchorEl)}
+                onClose={this.closeTheme}
+                PaperProps={{
+                  style: {
+                    background: '#bbbbbb',
+                    width: 56
+                  },
+                }}
+          >
+
+            <MenuItem key='light' onClick={this.setColor.bind(this, 'terminal-wrap light')}>
+              <ListItemIcon className="icon-button white">
+                <FiberManualRecordRoundedIcon className="icon-light"/>
+              </ListItemIcon>
+            </MenuItem>
+            <MenuItem key='dark' onClick={this.setColor.bind(this, 'terminal-wrap dark')}>
+              <ListItemIcon>
+                <FiberManualRecordRoundedIcon className="icon-dark"/>
+              </ListItemIcon>
+            </MenuItem>
+            <MenuItem key='primary' onClick={this.setColor.bind(this, 'terminal-wrap primary')}>
+              <ListItemIcon>
+                <FiberManualRecordRoundedIcon className="icon-primary"/>
+              </ListItemIcon>
+            </MenuItem>
+          </Menu>
+          {/*<IconButton aria-label="white" className="icon-button white" onClick={this.setColor.bind(this, 'terminal-wrap light')}>*/}
+            {/*<FiberManualRecordRoundedIcon />*/}
+          {/*</IconButton>*/}
+          {/*<IconButton aria-label="primary" className="icon-button primary" onClick={this.setColor.bind(this, 'terminal-wrap primary')}>*/}
+            {/*<FiberManualRecordRoundedIcon />*/}
+          {/*</IconButton>*/}
+          {/*<IconButton aria-label="dark" className="icon-button" onClick={this.setColor.bind(this, 'terminal-wrap dark')}>*/}
+            {/*<FiberManualRecordRoundedIcon />*/}
+          {/*</IconButton>*/}
           {
             this.state.run
               ?
