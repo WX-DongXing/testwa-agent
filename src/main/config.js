008fc9b536a717f3e39c9da3238bd033605da419
xd <xingdong.com@live.com>
2018-10-31 18:25:22 +0800

    Repaired login url and adaptive Linux system

--- src/main/config.js
+++ src/main/config.js
@@ -1,5 +1,5 @@
-export const ENV = 'test'
-export const LOGIN_URL = 'http://api.test.testwa.com/v1/auth/login'
+// export const ENV = 'test'
+// export const LOGIN_URL = 'http://api.test.testwa.com/v1/auth/login'
 
-// export const ENV = 'prod'
-// export const LOGIN_URL = 'https://api.testwa.com/v1/auth/login'
+export const ENV = 'prod'
+export const LOGIN_URL = 'http://api.testwa.com/v1/auth/login'
